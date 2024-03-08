43ef445409cb9e1b6f37b591dca1a00371badbdb
김다희 <dhmonukim24@gmail.com>
2024-03-08 18:01:35 +0900

    chore: 하위 프로젝트 생성

---
+++ backend/payment/src/main/java/site/soconsocon/payment/PaymentApplication.java
@@ -0,0 +1,14 @@
+package site.soconsocon.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+}
