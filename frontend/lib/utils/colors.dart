0dff9a389952da7dcbe474716cff846ac94355dc
kimahyeon <dhflxhdxhd@gmail.com>
2024-03-12 21:58:41 +0900

    feat: yaml 파일 설정 S10P22C207-288

---
+++ frontend/lib/utils/colors.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+/// 색상 종류를 정의하는 클래스
+/// author : 김아현
+class AppColors {
+
+  // MAIN COLOR
+  static const Color WHITE = Color(0xFFFFFFFF);
+  static const Color BLACK = Color(0xFF222222);
+  static const Color YELLOW = Color(0XFFF8D461);
+  static const Color PURPLE = Color(0XFFAE9DF5);
+  static const Color GREEN = Color(0XFFB7E786);
+  static const Color PINK = Color(0XFFFECEE4);
+  static const Color GRAY = Color(0XFFF4F4F8);
+  static const Color LIGHTBLUE = Color(0XFFECF0FF);
+
+  // GRAY
+  static const Color BACKGRAY = Color(0XFF818181);
+  static const Color GRAY25 = Color(0XFFFCFCFD);
+  static const Color GRAY50 = Color(0XFFF9FAFB);
+  static const Color GRAY100 = Color(0XFFF2F4F7);
+  static const Color GRAY200 = Color(0XFFE4E7EC);
+  static const Color GRAY300 = Color(0XFFD0D5DD);
+  static const Color GRAY400= Color(0XFF98A2B3);
+  static const Color GRAY500= Color(0XFF667085);
+  static const Color GRAY600= Color(0XFF475467);
+  static const Color GRAY700= Color(0XFF344054);
+  static const Color GRAY800= Color(0XFF1D2939);
+  static const Color GRAY900= Color(0XFF101828);
+
+  // YELLOW
+  static const Color WARNING25 = Color(0XFFFFFCF5);
+  static const Color WARNING50 = Color(0XFFFFFAEB);
+  static const Color WARNING100 = Color(0XFFFEF0C7);
+  static const Color WARNING200 = Color(0XFFFEDF89);
+  static const Color WARNING300 = Color(0XFFFEC84B);
+  static const Color WARNING400 = Color(0XFFFDB022);
+
+  // GREEN
+  static const Color SUCCESS300 = Color(0XFF6CE9A6);
+  static const Color SUCCESS400 = Color(0XFF32D583);
+  static const Color SUCCESS500 = Color(0XFF12B76A);
+  static const Color SUCCESS600 = Color(0XFF039855);
+
+  // BLUE
+  static const Color BLUE500 = Color(0XFF2E90FA);
+
+  // RED
+  static const Color ERROR100 = Color(0XFFFEE4E2);
+  static const Color ERROR200 = Color(0XFFFECDCA);
+  static const Color ERROR300 = Color(0XFFFDA29B);
+  static const Color ERROR400 = Color(0XFFF97066);
+  static const Color ERROR500 = Color(0XFFF04438);
+  static const Color ERROR600 = Color(0XFFD92D20);
+
+  // INDIGO
+  static const Color INDIGO200 = Color(0XFFC7D7FE);
+  static const Color INDIGO300 = Color(0XFFA4BCFD);
+  static const Color INDIGO400 = Color(0XFF8098F9);
+  static const Color INDIGO500 = Color(0XFF6172F3);
+  static const Color INDIGO600 = Color(0XFF444CE7);
+}
