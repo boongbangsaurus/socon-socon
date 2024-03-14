2d46f265a16f6b6b8e1733e22e33888d35ac6467
김온유 <gkc04075@gmail.com>
2024-03-14 17:59:03 +0900

    chofeat: tag ãbutton 위젯 생성

---
+++ frontend/lib/widgets/tag_buttons.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class TagButtons extends StatefulWidget {
+  const TagButtons({super.key});
+
+  @override
+  State<TagButtons> createState() => _TagButtonsState();
+}
+
+class _TagButtonsState extends State<TagButtons> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
