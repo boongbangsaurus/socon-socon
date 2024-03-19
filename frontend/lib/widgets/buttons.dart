651a5a6ed1b30609ddaaf1b0b63d53d03c6e2730
thagyun <thagyun@naver.com>
2024-03-20 07:10:03 +0900

    refactor: edit button class S10P22C207-204

--- frontend/lib/widgets/buttons.dart
+++ frontend/lib/widgets/buttons.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:socon/utils/colors.dart';
 import 'package:socon/utils/fontSizes.dart';
 
-/// 버튼들을 정의하는 클래스
+/// 버튼 정의 클래스
 /// author: 탁하윤
 
-// 큰 버튼
+/// 노란색 큰 버튼
+/// text: [필수] 버튼 텍스트 지정
+/// onPressed: [필수] 버튼 onPressed 함수
 class LargeButton extends StatelessWidget {
   LargeButton({Key? key, required this.text, required this.onPressed})
       : super(key: key);
@@ -15,23 +17,38 @@ class LargeButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ButtonTheme(
-        child: ElevatedButton(
-      style: ElevatedButton.styleFrom(backgroundColor: AppColors.YELLOW),
-      onPressed: onPressed,
-      child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              Text(
-                text,
-                style: const TextStyle(
-                    color: AppColors.WHITE,
-                    fontSize: FontSizes.XSMALL,
-                    fontWeight: FontWeight.bold),
-              )
-            ],
-          )),
-    ));
+    // return ButtonTheme(
+    //   height: 60,
+    //     child: ElevatedButton(
+    //   style: ElevatedButton.styleFrom(backgroundColor: AppColors.YELLOW),
+    //   onPressed: onPressed,
+    //   child: Padding(
+    //       padding: const EdgeInsets.all(20.0),
+    //       child: Row(
+    //         children: [
+    //           Text(
+    //             text,
+    //             style: const TextStyle(
+    //                 color: AppColors.WHITE,
+    //                 fontSize: FontSizes.XSMALL,
+    //                 fontWeight: FontWeight.bold),
+    //           )
+    //         ],
+    //       )),
+    // ));
+    return SizedBox(
+      height: 60,
+      width: 330,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                color: AppColors.WHITE,
+                fontSize: FontSizes.XSMALL,
+                fontWeight: FontWeight.bold)),
+        style: ElevatedButton.styleFrom(backgroundColor: AppColors.YELLOW),
+      ),
+    );
   }
 }
