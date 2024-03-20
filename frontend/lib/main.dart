47e76876d625b845ceedd16d8d7e5142b40642ac
kimahyeon <dhflxhdxhd@gmail.com>
2024-03-20 13:10:36 +0900

    chore : mainScreen 분리

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,12 +6,9 @@ import 'package:socon/utils/responsive_utils.dart';
 import 'package:socon/view/atoms/searchBox.dart';
 import './utils/toast_utils.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
-
-// GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,60 +16,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // builder: FToastBuilder(),
-      // navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Socon',
       theme: ThemeData(
-          // colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber),
-          // primaryColor: AppColors.mainYellow
-          // useMaterial3: true, // 이 줄을 주석 처리하거나 삭제하여 사용하시는 버전에 맞게 설정하세요.
-          ),
-      home: Scaffold(
-        backgroundColor: AppColors.WHITE,
-        body: Container(
-          width: ResponsiveUtils.getWidthPercent(context, 100),
-          height: ResponsiveUtils.getHeightPercent(context, 100),
-          // decoration: BoxDecoration(
-          //   border: Border.all(
-          //     width:1.0,
-          //     color: AppColors.WHITE
-          //   )
-          // ),
-          child: Column(
-            // 괄호 추가
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SearchBox(),
-              Text(
-                '소콘소콘',
-                style: TextStyle(
-                  fontFamily: "BagelFatOne",
-                  fontSize: ResponsiveUtils.calculateResponsiveFontSize(
-                      context, FontSizes.LOGO),
-                  color: AppColors.WHITE,
-                ),
-              ),
-              Text(
-                '소소한 우리 동네 기프티콘',
-                style: TextStyle(
-                  fontFamily: "Pretendard",
-                  fontWeight: FontWeight.w500,
-                  fontSize: ResponsiveUtils.calculateResponsiveFontSize(
-                      context, FontSizes.XSMALL),
-                  color: AppColors.WHITE,
-                ),
-              ),
-              // Container(
-              //     width: ResponsiveUtils.getHeightWithPixels(context, 200),
-              //     height: ResponsiveUtils.getHeightWithPixels(context, 60),
-              //     color: AppColors.INDIGO200),
-            ],
-          ), // 괄호 추가
-        ),
+        // colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber),
+        // primaryColor: AppColors.mainYellow
+        // useMaterial3: true, // 이 줄을 주석 처리하거나 삭제하여 사용하시는 버전에 맞게 설정하세요.
       ),
-
+      home: MainScreen(),
     );
   }
 }
@@ -85,93 +36,27 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreen extends State<MainScreen> {
-  // final GlobalKey<_ToastState> _toastKey = GlobalKey<_ToastState>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Toast"),
+      appBar: AppBar(
+        title: Text("Flutter Toast"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SearchBox(),
+            ElevatedButton(
+              child: Text("Show Toast"),
+              onPressed: () {
+                ToastUtil.showCustomToast(context, "availableSocon");
+              },
+            ),
+          ],
         ),
-        body: Center(
-          child: ElevatedButton(
-            child: Text("Show Toast"),
-            onPressed: () {
-              ToastUtil.showCustomToast(context, "availableSoco");
-            },
-          ),
-        )
+      ),
     );
   }
 }
-
-
-
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Socon',
-//       theme: ThemeData(
-//           // colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber),
-//           // primaryColor: AppColors.mainYellow
-//           // useMaterial3: true, // 이 줄을 주석 처리하거나 삭제하여 사용하시는 버전에 맞게 설정하세요.
-//           ),
-//       home: Scaffold(
-//         backgroundColor: AppColors.YELLOW,
-//         body: Center(
-//           child: ElevatedButton(
-//             onPressed: () {
-//               showToast(context);
-//             },
-//             child: Text('Show Toast'),
-//           ),
-//         )
-//         // body: Container(
-//         //   width: ResponsiveUtils.getWidthPercent(context, 100),
-//         //   height: ResponsiveUtils.getHeightPercent(context, 100),
-//         //   // decoration: BoxDecoration(
-//         //   //   border: Border.all(
-//         //   //     width:1.0,
-//         //   //     color: AppColors.WHITE
-//         //   //   )
-//         //   // ),
-//         //   child: Column(
-//         //     // 괄호 추가
-//         //     mainAxisAlignment: MainAxisAlignment.center,
-//         //     crossAxisAlignment: CrossAxisAlignment.center,
-//         //     children: [
-//         //       Text(
-//         //         '소콘소콘',
-//         //         style: TextStyle(
-//         //           fontFamily: "BagelFatOne",
-//         //           fontSize: ResponsiveUtils.calculateResponsiveFontSize(
-//         //               context, FontSizes.LOGO),
-//         //           color: AppColors.WHITE,
-//         //         ),
-//         //       ),
-//         //       Text(
-//         //         '소소한 우리 동네 기프티콘',
-//         //         style: TextStyle(
-//         //           fontFamily: "Pretendard",
-//         //           fontWeight: FontWeight.w500,
-//         //           fontSize: ResponsiveUtils.calculateResponsiveFontSize(
-//         //               context, FontSizes.XSMALL),
-//         //           color: AppColors.WHITE,
-//         //         ),
-//         //       ),
-//         //       // Container(
-//         //       //     width: ResponsiveUtils.getHeightWithPixels(context, 200),
-//         //       //     height: ResponsiveUtils.getHeightWithPixels(context, 60),
-//         //       //     color: AppColors.INDIGO200),
-//         //     ],
-//         //   ), // 괄호 추가
-//         // ),
-//       ),
-//     );
-//   }
-// }
