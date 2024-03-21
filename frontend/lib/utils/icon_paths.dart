c01e329557bb7ddcfae05f78430fb5f64ff68987
탁하윤 <thagyun@naver.com>
2024-03-21 15:15:22 +0900

    feat: icon palette component S10P22C207-281

--- frontend/lib/utils/icon_paths.dart
+++ frontend/lib/utils/icon_paths.dart
@@ -1,4 +1,7 @@
 const Map<String, String> iconPaths = {
   'search': 'assets/icons/search.svg',
   'filter': 'assets/icons/filter.svg',
+  'calendar': 'assets/icons/calendar.svg',
+  'home': 'assets/icons/home.svg',
+  'notifications': 'assets/icons/notifications.svg',
 };
