9462d428482386bdec2973b8cf14cb3494f92f31
kimahyeon <dhflxhdxhd@gmail.com>
2024-03-21 10:44:27 +0900

    feat: iconLoader 수정 및 imageLoader 구성 완 S10P22C207-211
    - iconLoader 너비, 높이, 패딩 생성자 정의
    - url에 해당하는 이미지 업로드하는 imageLoader 구현 완

--- frontend/lib/view/atoms/imageLoader.dart
+++ frontend/lib/view/atoms/imageLoader.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:socon/utils/responsive_utils.dart';
 
 class ImageLoader extends StatelessWidget {
   final String imageUrl;
-
+  final double borderRadius;
   // const ImageLoader({super.key, required this.imageUrl, required this.width, required this.height, required this.radius});
-  const ImageLoader({super.key, required this.imageUrl});
+  const ImageLoader({super.key, required this.imageUrl, this.borderRadius = 0});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image(
-        image: NetworkImage(imageUrl),
-        width: ResponsiveUtils.getWidthWithPixels(context, 77),
-        height: ResponsiveUtils.getHeightWithPixels(context, 77),
-      )
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius), // 이미지 테두리와 일치시키기 위해 추가
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.fill,
+      ),
     );
   }
-
-
 }
