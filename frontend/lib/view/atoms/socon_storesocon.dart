820fd62d93f42c447a4ff35cacac7919f426b6df
김온유 <gkc04075@gmail.com>
2024-03-21 17:57:42 +0900

    feat: 소콘 카드 컴포넌트 수정 S10P22C207-208

--- frontend/lib/view/atoms/socon_storesocon.dart
+++ frontend/lib/view/atoms/socon_storesocon.dart
@@ -5,28 +5,37 @@ import 'tag_icon.dart';
 
 class StoreSoconLists extends StatelessWidget {
   final String soconName;
-  final String storeName;
-  final String dueDate;
+  final bool isMain;
+  final int maxQuantity;    // 설정된 최대 발행량
+  final int issuedQuantity;   // 현재 발행 개수
+  final int price;     // 상품가격(정가)
+  final bool isDicounted;
+  final int discountedPrice;    // 할인된 가격. 없을 경우 null
   final String imageUrl;
 
   const StoreSoconLists({
     super.key,
     required this.soconName,
-    required this.storeName,
-    required this.dueDate,
+    required this.isMain,
+    required this.maxQuantity,
+    required this.issuedQuantity,
+    required this.price,
+    required this.isDicounted,
+    required this.discountedPrice,
     required this.imageUrl,
   });
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width / 2;
-
+    final remainQuantity = maxQuantity - issuedQuantity;
+    final discountPercent = isDicounted ? ((price - discountedPrice) / price * 100).toStringAsFixed(0) : '0';
     return Container(
       width: screenWidth,
       margin: EdgeInsets.all(5),
       padding: EdgeInsets.only(top: 20),
       decoration: BoxDecoration(
-          color: Colors.white,
+          color: isMain ? Colors.lightGreenAccent : Colors.white,
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
@@ -41,37 +50,37 @@ class StoreSoconLists extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
+            margin: EdgeInsets.symmetric(horizontal: 15),
             child: Row(
               children: [
                 TagIcon(buttonText: 'NEW', buttonColor: Color(0xffFBBC05), buttonTextColor: Colors.white),
-                TagIcon(buttonText: 'NEW', buttonColor: Color(0xffFEF4444), buttonTextColor: Colors.white),
+                TagIcon(buttonText: 'SALE', buttonColor: Color(0xffFEF4444), buttonTextColor: Colors.white),
               ],
             ),
           ),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
+            margin: EdgeInsets.symmetric(horizontal: 15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       soconName,
+                      textAlign: TextAlign.start,
                       style: TextStyle(
                         fontSize: 22,
                         fontWeight: FontWeight.bold,
                         color: Colors.black, ),
                     ),
                     Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      padding: EdgeInsets.only(bottom: 10),
+                      padding: EdgeInsets.only(bottom: 20),
                       child: Text(
-                        '잔여수량 ',
+                        '잔여수량 $remainQuantity',
                         style: TextStyle(
-                            fontSize: 13,
+                            fontSize: 12,
                             color: Colors.black),
-                        textAlign: TextAlign.left,
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                       ),
@@ -79,10 +88,15 @@ class StoreSoconLists extends StatelessWidget {
                   ],
                 ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text('20%'),
-                    Text('2,400원'),
-                    Text('3,000원'),
+                    if (isDicounted) ...[ // 할인된 경우
+                      Text('($discountPercent%)', style: TextStyle(fontSize: 12, color: Colors.red), ),
+                      Text('$discountedPrice원', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red)),
+                      Text('$price원', style: TextStyle(fontSize: 12, decoration: TextDecoration.lineThrough, fontWeight: FontWeight.bold)),
+                    ] else ...[ // 할인되지 않은 경우
+                      Text('$price원', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87)),
+                    ]
                   ],
                 )
               ],
