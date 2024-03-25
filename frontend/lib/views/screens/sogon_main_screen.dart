a19dd5f851ae3c82c28741678ed0312d7db3aec7
탁하윤 <thagyun@naver.com>
2024-03-25 17:30:46 +0900

    test: modal test code S10P22C207-198

--- frontend/lib/views/screens/sogon_main_screen.dart
+++ frontend/lib/views/screens/sogon_main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:socon/utils/fontSizes.dart';
+import 'package:socon/views/atoms/buttons.dart';
+import 'package:socon/views/atoms/modal.dart';
 
 class SogonMainScreen extends StatefulWidget {
   const SogonMainScreen({super.key});
@@ -11,14 +13,48 @@ class SogonMainScreen extends StatefulWidget {
 }
 
 class _SogonMainScreen extends State<SogonMainScreen> {
+  final bool trigger = true;
+  @override
+  void initState() {
+    super.initState();
+    if (trigger) {
+      // initState는 비동기 코드를 직접 실행할 수 없으므로, 다음 프레임에서 대화상자를 표시하기 위한 코드를 스케줄합니다.
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        CustomModal.showCustomDialog(
+          context: context,
+          title: 'Test',
+          content: 'This is a test dialog.',
+          actions: <Widget>[
+            Expanded(
+              child: Row(children: [
+                BasicButton(
+                    text: 'HI',
+                    btnSize: 's',
+                    onPressed: () => Navigator.of(context).pop()),
+                BasicButton(
+                    text: 'Hello',
+                    btnSize: 's',
+                    onPressed: () => Navigator.of(context).pop()),
+              ]),
+            )
+          ],
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "소곤",
-          style: TextStyle(fontSize: FontSizes.LARGE),
-        ),
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              "소곤",
+              style: TextStyle(fontSize: FontSizes.LARGE),
+            ),
+          ),
+        ],
       ),
     );
   }
