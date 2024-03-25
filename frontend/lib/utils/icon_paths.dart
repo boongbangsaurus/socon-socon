496e1812f97a1cdab406e3720410174e81a0ad15
kimahyeon <dhflxhdxhd@gmail.com>
2024-03-25 10:53:51 +0900

    fix: 프론트엔드 build 에러

--- frontend/lib/utils/icon_paths.dart
+++
@@ -1,9 +0,0 @@
-const Map<String, String> iconPaths = {
-  'search': 'assets/icons/search.svg',
-  'filter': 'assets/icons/filter.svg',
-  'calendar': 'assets/icons/calendar.svg',
-  'home': 'assets/icons/home.svg',
-  'notifications': 'assets/icons/notifications.svg',
-  'heart_empty': 'assets/icons/heart_empty.svg',
-};
-  // 'heart_fill': 'assets/icons/heart_fill.svg',
