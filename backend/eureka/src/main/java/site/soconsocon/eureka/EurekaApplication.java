2106e08a1bdecdb3f4f131de9d83f632c3272f71
김다희 <dhmonukim24@gmail.com>
2024-03-25 18:02:15 +0900

    chore: spring cloud config 의존성 추가

--- backend/eureka/src/main/java/site/soconsocon/eureka/EurekaApplication.java
+++ backend/eureka/src/main/java/site/soconsocon/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package site.soconsocon.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
