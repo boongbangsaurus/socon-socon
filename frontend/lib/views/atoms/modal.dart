5c6285ffdce39032c398e8e1fb9a7dbdf256564b
탁하윤 <thagyun@naver.com>
2024-03-26 09:23:17 +0900

    feat: custom modal S10P22C207-198

--- frontend/lib/views/atoms/modal.dart
+++ frontend/lib/views/atoms/modal.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:socon/utils/colors.dart';
+import 'package:socon/utils/fontSizes.dart';
 
+/// [CustomModal]
+/// 커스텀 모달 클래스
+/// [showCustomDialog] 함수 정의
+/// @param String [title] 팝업 타이틀
+/// @param String [content] 팝업 내용
+/// @param List<Widget> [actions] 버튼 리스트 위젯
+/// @return custom AlertDialog
 class CustomModal {
   static AlertDialog showCustomDialog({
     required String title,
@@ -7,9 +16,18 @@ class CustomModal {
     required List<Widget> actions,
   }) {
     return AlertDialog(
-      title: Text(title),
-      content: Text(content),
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style:
+            TextStyle(fontSize: FontSizes.XLARGE, fontWeight: FontWeight.bold),
+      ),
+      content: Text(content,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: FontSizes.XXXSMALL)),
       actions: actions,
+      backgroundColor: AppColors.WHITE,
+      surfaceTintColor: Colors.transparent,
     );
   }
 }
--- frontend/lib/views/screens/sogon_main_screen.dart
+++ frontend/lib/views/screens/sogon_main_screen.dart
@@ -13,36 +13,6 @@ class SogonMainScreen extends StatefulWidget {
 }
 
 class _SogonMainScreen extends State<SogonMainScreen> {
-  // final bool trigger = true;
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   if (trigger) {
-  //     // initState는 비동기 코드를 직접 실행할 수 없으므로, 다음 프레임에서 대화상자를 표시하기 위한 코드를 스케줄합니다.
-  //     WidgetsBinding.instance.addPostFrameCallback((_) {
-  //       CustomModal.showCustomDialog(
-  //         context: context,
-  //         title: 'Test',
-  //         content: 'This is a test dialog.',
-  //         actions: <Widget>[
-  //           Expanded(
-  //             child: Row(children: [
-  //               BasicButton(
-  //                   text: 'HI',
-  //                   btnSize: 's',
-  //                   onPressed: () => Navigator.of(context).pop()),
-  //               BasicButton(
-  //                   text: 'Hello',
-  //                   btnSize: 's',
-  //                   onPressed: () => Navigator.of(context).pop()),
-  //             ]),
-  //           )
-  //         ],
-  //       );
-  //     });
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,7 +39,10 @@ class _SogonMainScreen extends State<SogonMainScreen> {
                                 text: '아니오',
                                 btnSize: 's',
                                 color: 'gray',
-                                onPressed: () {},
+                                textColor: 'black',
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
                               )),
                               SizedBox(
                                 width: 10.0,
@@ -78,7 +51,9 @@ class _SogonMainScreen extends State<SogonMainScreen> {
                                   child: BasicButton(
                                 text: '예',
                                 btnSize: 's',
-                                onPressed: () {},
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
                               ))
                             ],
                           )
