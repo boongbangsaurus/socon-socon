9409404fd78cda95b013598666f471a81706adfd
dohwi <ehgnl97722@ajou.ac.kr>
2024-03-27 17:54:28 +0900

    feat: 주문 출금 S10P22C207-506

--- backend/user/src/main/java/site/soconsocon/auth/controller/AccountController.java
+++ backend/user/src/main/java/site/soconsocon/auth/controller/AccountController.java
@@ -3,10 +3,7 @@ package site.soconsocon.auth.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.soconsocon.auth.domain.dto.request.DepositRequestDto;
 import site.soconsocon.auth.domain.dto.request.WithdrawRequestDto;
 import site.soconsocon.auth.exception.AccountException;
@@ -24,7 +21,7 @@ public class AccountController {
     private final AccountService accountService;
 
     //소콘머니 증가
-    @PostMapping("/deposit")
+    @PutMapping("/deposit")
     public ResponseEntity deposit(@RequestBody DepositRequestDto depositRequestDto) throws MemberException, AccountException {
         accountService.deposit(depositRequestDto);
         return ResponseEntity.ok().body(MessageUtils.success());
@@ -32,7 +29,7 @@ public class AccountController {
 
 
     //소콘머니 출금
-    @PostMapping("/withdraw")
+    @PutMapping("/withdraw")
     public ResponseEntity withdraw(@RequestBody WithdrawRequestDto withdrawRequestDto) throws AccountException, MemberException {
         accountService.withdraw(withdrawRequestDto);
         return ResponseEntity.ok().body(MessageUtils.success());
