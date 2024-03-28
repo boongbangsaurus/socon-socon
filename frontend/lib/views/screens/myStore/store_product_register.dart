3a6e08e5e962eda8e1876fcfed66265088601666
김온유 <gkc04075@gmail.com>
2024-03-28 11:41:54 +0900

    feat: 점포 메뉴등록 퍼블리싱 S10P22C207-475

--- frontend/lib/views/screens/myStore/store_product_register.dart
+++ frontend/lib/views/screens/myStore/store_product_register.dart
@@ -1,16 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 import 'package:socon/utils/colors.dart';
 import 'package:socon/utils/fontSizes.dart';
 import 'package:socon/utils/responsive_utils.dart';
+import 'package:socon/utils/toast_utils.dart';
 import 'package:socon/views/atoms/buttons.dart';
 import 'package:socon/views/atoms/input_form.dart';
 import 'package:socon/views/screens/myStore/product_register_toast.dart';
 
 
-class ProductRegister extends StatelessWidget {
+
+class ProductRegister extends StatefulWidget {
   final int storeId;
 
-  const ProductRegister({super.key, required this.storeId});
+  ProductRegister({super.key, required this.storeId});
+
+  @override
+  State<ProductRegister> createState() => _ProductRegisterState();
+}
+
+class _ProductRegisterState extends State<ProductRegister> {
+  var userImage;
+  String menuName = '';
+  String introLine = '';
+  String productPrice = '';
+
+  bool isInputValid() {
+    return menuName.isNotEmpty && userImage != null && introLine.isNotEmpty && productPrice.isNotEmpty;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,21 +53,93 @@ class ProductRegister extends StatelessWidget {
 
                       CustomInputField(
                         labelText: '메뉴 이름',
-                        onChanged: (value) => {},
+                        onChanged: (value) => {
+                          setState(() {
+                             menuName = value;
+                             // viewModel에 업데이트
+                          })
+                        },
                       ),
 
                       Text('상품 사진을 추가해 주세요',
                         style: TextStyle(fontSize: FontSizes.SMALL, fontWeight: FontWeight.bold),),
+                      Row(
+                        children: [
+                          // 이미지를 보여줄 box
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 20),
+                            width: 80, // 이미지 박스의 너비 설정
+                            height: 80, // 이미지 박스의 높이 설정
+                            decoration: BoxDecoration(
+                              color: AppColors.GRAY200,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: userImage != null
+                                ? Stack(
+                              // alignment: Alignment.topRight,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.file(
+                                    userImage,
+                                    width: 80,
+                                    height: 80,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                  top: -10,
+                                  right: -10,
+                                  child:
+                                    IconButton(
+                                      icon: Icon(Icons.close, color: Colors.white, size: 15,),
+                                      onPressed: () {
+                                        setState(() {
+                                          userImage = null;
+                                        });
+                                      },
+                                    ),
+                                ),
+                              ],
+                            )
+                                : IconButton(
+                              onPressed: () async {
+                                var picker = ImagePicker();
+                                var image = await picker.pickImage(source: ImageSource.gallery);
+                                if (image != null) {
+                                  setState(() {
+                                    userImage = File(image.path); // 이미지 선택
+                                  });
+                                }
+                              },
+                              icon: Icon(Icons.add),
+                              color: Colors.black,
+                            ),
+                          ),
+
+
+                        ],
+                      ),
                       SizedBox(height: 10,),
 
                       CustomInputField(
                         labelText: '한줄 소개',
-                        onChanged: (value) => {},
+                        onChanged: (value) => {
+                          setState(() {
+                            introLine = value;
+                            // viewModel에 업데이트
+                          })
+                        },
                       ),
 
                       CustomInputField(
                         labelText: '상품 가격',
-                        onChanged: (value) => {},
+                        onChanged: (value) => {
+                          setState(() {
+                            productPrice = value;
+                            // viewModel에 업데이트
+                          })
+                        },
                       ),
 
                       Text('선택입력',
@@ -98,9 +190,13 @@ class ProductRegister extends StatelessWidget {
               text: '다음',
               color: 'yellow',
               onPressed: () => {
-                Navigator.push( context,
-                  MaterialPageRoute(builder: (context) => RegisterToastMsg(storeId: storeId)),
-                )
+                if(isInputValid()){
+                  Navigator.push( context,
+                    MaterialPageRoute(builder: (context) => RegisterToastMsg(storeId: widget.storeId)),
+                  ),
+                } else {
+                  ToastUtil.showCustomToast(context, 'filedAlert')
+                },
               }
             ),
           ],
