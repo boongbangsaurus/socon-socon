daa567b7e05cb2076ce336696c8c08661531699c
이유빈 <kgbn013@naver.com>
2024-03-30 21:09:27 +0900

    Revert "fix: 중간 업로드 S10P22C207-245"
    This reverts commit 885b6662373f88f9e12c11e9f3d4c1d898d2bcc0

--- backend/user/src/main/java/site/soconsocon/auth/repository/QnaRepository.java
+++
@@ -1,9 +0,0 @@
-package site.soconsocon.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import site.soconsocon.auth.domain.entity.jpa.Qna;
-
-@Repository
-public interface QnaRepository extends JpaRepository<Qna, Integer> {
-}
