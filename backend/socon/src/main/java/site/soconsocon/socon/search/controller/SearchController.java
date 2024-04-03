122eec5eaf2e73c92dd5a89266a09cd64e018817
김다희 <dhmonukim24@gmail.com>
2024-04-04 00:58:45 +0900

    chore: 주석 해제

--- backend/socon/src/main/java/site/soconsocon/socon/search/controller/SearchController.java
+++ backend/socon/src/main/java/site/soconsocon/socon/search/controller/SearchController.java
@@ -30,11 +30,9 @@ public class SearchController {
 
     @PostMapping("/me")
     public ResponseEntity sendNotification(
-            @RequestBody StoreNearMe storeNearMe){
-//        ,
-//        @RequestHeader("X-Authorization-Id") int memberId
-//        soconRedisService.findAndNotify(storeNearMe, memberId);
-        soconRedisService.findAndNotify(storeNearMe, 1);
+            @RequestBody StoreNearMe storeNearMe,
+            @RequestHeader("X-Authorization-Id") int memberId){
+        soconRedisService.findAndNotify(storeNearMe, memberId);
         return ResponseEntity.ok().body(MessageUtils.success());
     }
 
